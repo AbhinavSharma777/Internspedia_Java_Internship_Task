236239c25082f7754434369f42736d86615bbb1b
inwardking <90845787+inwardking@users.noreply.github.com>
2022-09-15 09:37:21 +0530

    Delete Java_tsk_1.java

--- Java_tsk_1.java
+++
@@ -1,7 +0,0 @@
-public class Java_tsk_1 {  
-public static void main (String args[])  {   
-char[] ch = {'J', 'A', 'V', 'A', ' ' ,'I', 'N', 'T', 'E' , 'R', 'N', 'S', 'H', 'I', 'P',' ', 'T', 'A', 'S', 'K'};    
-String string = new String(ch);  
-System.out.println(string);  
-}  
-} 
